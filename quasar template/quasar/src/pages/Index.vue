5bd72858fd932ca0c2f269105824bb60b355c335
LZ <zanli0718@gmail.com>
2021-08-22 19:31:55 +0800

    quasar template

---
+++ quasar template/quasar/src/pages/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import * as echarts from 'echarts'
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
---
+++ quasar template/quasar/src/pages/demo.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+  <div style="width: 500px; height: 500px;" ref="chartRef"></div>
+</div>
+</template>
+
+<script>
+
+//引入echarts
+import * as echarts from 'echarts'
+
+export default {
+  name: 'echarts',
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {
+    let myChart = echarts.init(this.$refs.chartRef);
+    myChart.setOption({
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    });
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
